bd8e6070c90a523872b75c19b129d391a2148b20
Caesar <monalisarz2934@gmail.com>
2026-04-09 10:07:53 +0700

    Update Auth and Middleware

--- AuthService/Middlewares/TokenBlacklistMiddleware.cs
+++ AuthService/Middlewares/TokenBlacklistMiddleware.cs
@@ -12,5 +12,27 @@ namespace AuthService.Middlewares
             _next = next;
         }
 
+        public async Task Invoke(HttpContext context, TokenCacheService tokenCache)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                var handler = new JwtSecurityTokenHandler();
+                if (handler.CanReadToken(token))
+                {
+                    var jwtToken = handler.ReadJwtToken(token);
+                    var jti = jwtToken.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Jti)?.Value;
+
+                    if (jti != null && await tokenCache.IsTokenBlacklistedAsync(jti))
+                    {
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        await context.Response.WriteAsync("Token đã bị thu hồi (Logged out).");
+                        return;
+                    }
+                }
+            }
+            await _next(context);
+        }
     }
 }
