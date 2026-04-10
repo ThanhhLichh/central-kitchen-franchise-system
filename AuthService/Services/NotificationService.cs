e93ff2d6c9d1f2ad8298a860f9fee8a670a76d16
Caesar <monalisarz2934@gmail.com>
2026-04-10 16:59:03 +0700

    Update Notification and TokenCache service

--- AuthService/Services/NotificationService.cs
+++ AuthService/Services/NotificationService.cs
@@ -4,6 +4,21 @@ namespace AuthService.Services
 {
     public class NotificationService
     {
-        
+        public async Task SendAccountCreatedNotificationAsync(string fcmToken, string username)
+        {
+            if (string.IsNullOrEmpty(fcmToken)) return;
+
+            var message = new Message()
+            {
+                Token = fcmToken,
+                Notification = new Notification
+                {
+                    Title = "Hệ Thống Franchise",
+                    Body = $"Tài khoản {username} của bạn đã được khởi tạo thành công."
+                }
+            };
+
+            await FirebaseMessaging.DefaultInstance.SendAsync(message);
+        }
     }
 }
